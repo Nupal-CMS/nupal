08f92af88bb1002d6355d0fa0d4cec497dc0b1ed
Taylor Evanson <dharkflower@gmail.com>
2023-05-28 00:30:24 -0600

    yeet

---
+++ modules/admin/admin.js
@@ -0,0 +1,6 @@
+import * as dotenv from 'dotenv'
+            dotenv.config()
+
+export const index = (req, res) => {
+    res.twig('index.html')
+}
--- modules/autoloader.js
+++ modules/autoloader.js
@@ -1,5 +1,12 @@
 import * as fs from 'node:fs/promises'
+
 import _ from 'lodash'
+import yaml from 'read-yaml-file'
+
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 import express from "express";
 const app = express.Router()
 
@@ -8,8 +15,18 @@ let modules = await fs.readdir('./modules')
     modules = _.reject(modules, o => ignore.indexOf(o) > -1)
 
 for(let mod of modules) {
-    let route = await import(`./${mod}/app.js`)
-    app.use('/' + mod, route.default)
-}
+    //app.use('/' + mod, route.default)
 
+    let pod = await import(__dirname + `/${mod}/${mod}.js`)
+    let routes = await yaml(__dirname + `/${mod}/${mod}.routing.yml`)
+
+    for(let key in routes) {
+        let route = routes[key]
+        let _controller = route.defaults._controller,
+            controller = _controller.split(':')[1]
+
+        app.use(route.path, pod[controller])
+    }
+
+}
 export default app;
