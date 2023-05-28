5e7cddd2983e7abddabf1a6d4fcc5dc39c033980
Taylor Evanson <dharkflower@gmail.com>
2023-05-27 23:45:57 -0600

    yeet

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 console.log("Nupal.");
-import * as dotenv from 'dotenv'
-            dotenv.config()
 
 import { getConfig } from '../core/config.js'
 const config = await getConfig('core.system')
 
+// SCSS
 import(`../views/${config.theme.machine_name}/scss/index.scss`)
