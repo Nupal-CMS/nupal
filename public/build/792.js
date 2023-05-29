83b4e415fc3b0bb05a9b31e928d6830198218a10
Taylor Evanson <dharkflower@gmail.com>
2023-05-29 17:51:51 -0600

    yeet

---
+++ public/build/792.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunknupal=self.webpackChunknupal||[]).push([[792],{792:(s,e,p)=>{s.exports=p.p+"bundle.min.css"}}]);
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ console.log("Nupal.");
 
 import { getConfig } from '../core/config.js'
 const config = await getConfig('core.system')
-// SCSS
-import(`${config.theme.machine_name}/scss/index.scss`)
+
+import('./index.scss')
