3468bb9dfe634b19a82e872b259248fe084b6a4c
Taylor Evanson <dharkflower@gmail.com>
2023-05-29 18:13:21 -0600

    yeet

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-console.log("Nupal.");
-
+console.log('Nupal.')
 import { getConfig } from '../core/config.js'
-const config = await getConfig('core.system')
+import './index.scss'
 
-import('./index.scss')
