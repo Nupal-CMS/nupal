a7c079be302b7f6adf21f94f8955cf7ed498b866
Taylor Evanson <dharkflower@gmail.com>
2023-06-01 09:04:23 -0600

    yeet

--- modules/nupal/nupal.js
+++ modules/nupal/nupal.js
@@ -1,26 +1,3 @@
 import express from 'express'
 const app = express()
-import Twig from 'twig'
-import client from '../../core/redis.js'
-import { getConfig } from '../../core/config.js'
-
-app.use(async (req, res, next) => {
-    req.client = client
-    req.theme = await client.hGet('theme', 'active')
-    req.config = await getConfig('core.system')
-
-    res.render = async (tpl, data = {}) => {
-
-        let path = `${process.env.MODULE_PATH}/${req.info.name}/src/templates`
-            data = Object.assign({}, data, { env: process.env }, { req: req })
-
-        await Twig.renderFile(`${path}/${tpl}.twig`, data, (err, html) => {
-            if(err) console.error('controller template error: ', err)
-            res.end(html)
-        })
-    }
-
-    next()
-})
-
 export default app
--- modules/nupal/src/middleware/config.js
+++ modules/nupal/src/middleware/config.js
@@ -1,3 +1,23 @@
+import Twig from 'twig'
+import client from '../../../../core/redis.js'
+import { getConfig } from '../../../../core/config.js'
+
 export default async (req, res, next) => {
+
+    req.client = client
+    req.theme = await client.hGet('theme', 'active')
+    req.config = await getConfig('core.system')
+
+    res.render = async (tpl, data = {}) => {
+
+        let path = `${process.env.MODULE_PATH}/${req.info.name}/src/templates`
+            data = Object.assign({}, data, { env: process.env }, { req: req })
+
+        await Twig.renderFile(`${path}/${tpl}.twig`, data, (err, html) => {
+            if(err) console.error('controller template error: ', err)
+            res.end(html)
+        })
+    }
+
     next()
 }
