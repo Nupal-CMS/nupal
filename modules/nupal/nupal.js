d0d7c5b3f849eed9e8e81b4966bdac3a642abcf1
Taylor Evanson <dharkflower@gmail.com>
2023-06-01 06:49:35 -0600

    yeet

--- modules/nupal/nupal.js
+++ modules/nupal/nupal.js
@@ -3,7 +3,6 @@ const app = express()
 import Twig from 'twig'
 import client from '../../core/redis.js'
 import { getConfig } from '../../core/config.js'
-import path from 'node:path'
 
 app.use(async (req, res, next) => {
     req.client = client
@@ -11,9 +10,9 @@ app.use(async (req, res, next) => {
     req.config = await getConfig('core.system')
 
     res.render = async (tpl, data = {}) => {
-        let path = `${process.env.MODULE_PATH}/${req.info.name}/src/templates`
 
-        data = Object.assign({}, data, { env: process.env }, { req: req })
+        let path = `${process.env.MODULE_PATH}/${req.info.name}/src/templates`
+            data = Object.assign({}, data, { env: process.env }, { req: req })
 
         await Twig.renderFile(`${path}/${tpl}.twig`, data, (err, html) => {
             if(err) console.error('controller template error: ', err)
